5a20d36d71ada4800009c295a06eb4ad0d61f0a7
Bart Simpson <bartsimpson7514@gmail.com>
2024-07-09 11:18:50 +0530

    added aptos wallet adapter

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Counter } from "./components/counter/Counter";
+import { WalletSelector } from "./components/WalletSelector";
 
 export default function IndexPage() {
-  return <Counter />;
+  return <WalletSelector/>
 }
 
 export const metadata: Metadata = {
